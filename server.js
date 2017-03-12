f83b9e95977c8ab4df9804a4f08d66632c8e10fc
Max Carlquist <tenkaklet@gmail.com>
2017-03-12 16:49:49 +0100

    first and final commit

---
+++ server.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+
+// log the http layer
+app.use(morgan('common'));
+
+// we're going to add some items to ShoppingList
+// so there's some data to look at
+ShoppingList.create('beans', 2);
+ShoppingList.create('tomatoes', 3);
+ShoppingList.create('peppers', 4);
+
+// Making Recipes
+Recipes.create("French Fries", ['potatoes', 'oil', 'salt']);
+Recipes.create("French Toast", ['bread', 'oil', 'salt', 'syrup']);
+Recipes.create("Salad", ['tomatoes', 'oil', 'salt', 'cucumber']);
+
+// when the root of this router is called with GET, return
+// all current ShoppingList items
+app.get('/shopping-list', (req, res) => {
+  res.json(ShoppingList.get());
+});
+
+// all Recipes
+app.get('/recipes', (req,res) => {
+  res.json(Recipes.get());
+});
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
